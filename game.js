376a607e06a0c872bab22d8595f0238a359d93eb
Tyler King <Tyler.19.king@gmail.com>
2022-03-09 23:40:17 -0500

    jQuery plus beginning game logic

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
-alert("test");
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+nextSequence();
+
+
+
+
+function nextSequence(){
+  var randomNumber = Math.floor(Math.random()*4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $('#'+randomChosenColour).fadeOut(100).fadeIn(100);
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+}
