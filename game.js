467646b1cf0a147f375c3a6917a60ad8529eb05a
Tyler King <Tyler.19.king@gmail.com>
2022-03-13 12:06:03 -0400

    finished game logic

--- game.js
+++ game.js
@@ -1,15 +1,76 @@
+var userClickedPattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
-nextSequence();
+var level = 0;
+var game = false;
+
+$(document).keypress(function(){
+  if(game==false){
+    $("#level-title").text("Level "+level);
+    nextSequence();
+    game=true;
+
+  }
+})
 
 
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    animatePress(userChosenColour);
+    playSound(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+});
 
+
+function startOver(){
+  level = 0;
+  gamePattern = [];
+  game = false;
+}
+
+function checkAnswer(currentLevel){
+  if(gamePattern[currentLevel]==userClickedPattern[currentLevel]){
+    //success
+    console.log("success")
+    if(userClickedPattern.length==gamePattern.length){
+      setTimeout(nextSequence, 1000);
+    }
+  }else{
+    console.log("failure");
+    var a2 = new Audio("sounds/wrong.mp3");
+    a2.play();
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    },200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function playSound(colour){
+  var audio = new Audio("sounds/" + colour + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour){
+  $("#"+currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("#"+currentColour).removeClass("pressed");
+  },100);
+}
 
 function nextSequence(){
+  userClickedPattern = [];
+
+  level++;
+  $("#level-title").text("Level "+level);
+
   var randomNumber = Math.floor(Math.random()*4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-  $('#'+randomChosenColour).fadeOut(100).fadeIn(100);
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();
+
+  $('#'+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
 }
